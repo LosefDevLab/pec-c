cac4152851e7435858ce8d422196d50f798aa9d4
XYLCS-Studio <158823035+XYLCS-Studio@users.noreply.github.com>
2024-10-05 06:11:43 +0000

    syntax_lib was complete plan

---
+++ engine-syntax_lib.cpp
@@ -0,0 +1,13 @@
+#include <map>
+using namespace std;
+map<string,string> synlib = {
+    //符号
+    {"@","kzfh"},
+    {"/","ysfh"},
+    {"/","kzfh"},
+    {"//","zs"},
+    {"<","ysfh"},
+    {">","ysfh"},
+    {"<","kzfh"},
+    {">","kzfh"}
+};
